0ac9d5fbdc19d6e46f53f78262bcebcdce180e35
Tim Stasse <tjstasse@agiledigital.com.au>
2018-07-12 17:31:40 +1000

    Updated config

--- config/config.env.js
+++ config/config.env.js
@@ -1,7 +1,19 @@
-module.exports = {
-  AWS_ACCOUNT_ID: '',
-  AMAZON_VENDOR_ID: '',
-  AMAZON_CLIENT_ID: '',
-  AMAZON_CLIENT_SECRET: '',
-  ALEXA_SKILL_ID: ''
-};
+const fs = require('fs');
+const path = require('path');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const dotenvPath = path.resolve(appDirectory, '.env');
+
+var dotenvFiles = [dotenvPath, `${dotenvPath}.local`].filter(Boolean);
+
+dotenvFiles.forEach(dotenvFile => {
+  if (fs.existsSync(dotenvFile)) {
+    require('dotenv-expand')(
+      require('dotenv').config({
+        path: dotenvFile
+      })
+    );
+  }
+});
+
+module.exports = process.env;
